53b5a6ecf2ad3cacdcfb32d0bdab0ed950432d60
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-02-13 21:56:26 +0000

    Fix typo

--- src/lua/LegacyBalanceMod_FileHooks.lua
+++ src/lua/LegacyBalanceMod_FileHooks.lua
@@ -2,4 +2,4 @@ g_legacyBalanceModRevision = 1
 g_legacyBalanceModBeta = 0
 
 -- Globals
-ModLoader.SetupFileHook("lua/Balance.lua", "lua/CompMod/Globals/Balance.lua", "post")
+ModLoader.SetupFileHook("lua/Balance.lua", "lua/LegacyBalanceMod/Globals/Balance.lua", "post")
