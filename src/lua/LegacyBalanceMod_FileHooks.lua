c1de09cd13c175d5662f067699746483c2a108cc
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-02-13 23:21:52 +0000

    Add Railgun falloff damage

--- src/lua/LegacyBalanceMod_FileHooks.lua
+++ src/lua/LegacyBalanceMod_FileHooks.lua
@@ -3,3 +3,7 @@ g_legacyBalanceModBeta = 0
 
 -- Globals
 ModLoader.SetupFileHook("lua/Balance.lua", "lua/LegacyBalanceMod/Globals/Balance.lua", "post")
+
+-- Weapons
+-- Weapons/Marine
+ModLoader.SetupFileHook("lua/Weapons/Marine/Railgun.lua", "lua/LegacyBalanceMod/Weapons/Marine/Railgun.lua", "post")
