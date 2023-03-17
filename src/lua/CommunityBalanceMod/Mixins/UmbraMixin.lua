42c839e5b14039cfde78b05a37c562dd0aa75c6c
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-03-17 16:33:08 +0000

    Check shell level when deleting umbra

--- src/lua/CommunityBalanceMod/Mixins/UmbraMixin.lua
+++ src/lua/CommunityBalanceMod/Mixins/UmbraMixin.lua
@@ -2,7 +2,7 @@ if Server then
     function UmbraMixin:SetOnFire()
         -- CommunityBalanceMod
         -- Don't delete umbra on players when they're set on fire if we have Resilience at any shell level
-        if not self:GetHasUpgrade(kTechId.Resilience) then
+        if not self:GetHasUpgrade(kTechId.Resilience) and self:GetShellLevel() > 0 then
             self.dragsUmbra = false
             self.timeUmbraExpires = 0
         end
