1c23a5315bd8119c34d8d94afcfac1ac9530d0e9
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-03-17 16:26:20 +0000

    Add missing 'then'

--- src/lua/CommunityBalanceMod/Mixins/UmbraMixin.lua
+++ src/lua/CommunityBalanceMod/Mixins/UmbraMixin.lua
@@ -2,7 +2,7 @@ if Server then
     function UmbraMixin:SetOnFire()
         -- CommunityBalanceMod
         -- Don't delete umbra on players when they're set on fire if we have Resilience at any shell level
-        if not self:GetHasUpgrade(kTechId.Resilience)
+        if not self:GetHasUpgrade(kTechId.Resilience) then
             self.dragsUmbra = false
             self.timeUmbraExpires = 0
         end
