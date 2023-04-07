3735e67838e87b433cd1161342dc3cc6e1204dbe
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-04-07 11:25:01 +0100

    Simplify AlienWeaponEffects, instead of replacing the file just update the parts we need

--- src/lua/CommunityBalanceMod_FileHooks.lua
+++ src/lua/CommunityBalanceMod_FileHooks.lua
@@ -43,7 +43,7 @@ ModLoader.SetupFileHook("lua/TechTreeConstants.lua", "lua/CommunityBalanceMod/Te
 
 -- Weapons
 -- Weapons/Alien
-ModLoader.SetupFileHook("lua/AlienWeaponEffects.lua", "lua/CommunityBalanceMod/Weapons/Alien/AlienWeaponEffects.lua", "replace")
+ModLoader.SetupFileHook("lua/AlienWeaponEffects.lua", "lua/CommunityBalanceMod/Weapons/Alien/AlienWeaponEffects.lua", "post")
 ModLoader.SetupFileHook("lua/Weapons/Alien/Shockwave.lua", "lua/CommunityBalanceMod/Weapons/Alien/Shockwave.lua", "post")
 
 -- Weapons/Marine
