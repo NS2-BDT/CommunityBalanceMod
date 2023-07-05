aba039ce4740e33f4e134670fe05ee90131b2b70
Dreyal <dreyal42@gmail.com>
2023-07-05 23:28:33 +0200

    Advanced Support Name Change

--- src/lua/CommunityBalanceMod/Locale/Locale.lua
+++ src/lua/CommunityBalanceMod/Locale/Locale.lua
@@ -1,4 +1,5 @@
 if Client then
     Locale.substitutions["RESILIENCE"] = "Heat Plating"
     Locale.substitutions["RESILIENCE_TOOLTIP"] = "Reduced damage from explosive, fire, and energy based weaponry"
+    Locale.substitutions["ADVANCED_SUPPORT"] = "Advanced Support"
 end
