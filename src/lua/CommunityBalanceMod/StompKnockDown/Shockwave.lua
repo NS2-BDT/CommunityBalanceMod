ccc4eba1ea902948cb0071e665c524ba8b49c88f
Dreyal <dreyal42@gmail.com>
2023-10-22 20:21:17 +0200

    prevent stomp affecting marines in the air

--- src/lua/CommunityBalanceMod/StompKnockDown/Shockwave.lua
+++ src/lua/CommunityBalanceMod/StompKnockDown/Shockwave.lua
@@ -114,24 +114,23 @@ function Shockwave:Detonate()
         for _, enemy in ipairs(enemies) do
             local enemyId = enemy:GetId()
             if enemy:GetIsAlive() and not table.contains(self.damagedEntIds, enemyId) and math.abs(enemy:GetOrigin().y - groundTrace.endPoint.y) < 0.8 then
-                self:DoDamage(kStompDamage, enemy, enemy:GetOrigin(), GetNormalizedVector(enemy:GetOrigin() - groundTrace.endPoint), "none")
-                table.insert(self.damagedEntIds, enemyId)
+                
 
                 local hasGroundMove = HasMixin(enemy, "GroundMove")
                 
+                -- only hit marines on the ground
                 if not hasGroundMove or enemy:GetIsOnGround() then
                     self:TriggerEffects("shockwave_hit", { effecthostcoords = enemy:GetCoords() })
+
+                    self:DoDamage(kStompDamage, enemy, enemy:GetOrigin(), GetNormalizedVector(enemy:GetOrigin() - groundTrace.endPoint), "none")
+                     table.insert(self.damagedEntIds, enemyId)
+
+                     if HasMixin(enemy, "Webable") then
+                        enemy:SetWebbed(kWebbedDuration, true)
+                    end
+
                 end
 
-                -- CommunityBalanceMod: Don't stun players, web them
-                -- 
-                -- if HasMixin(enemy, "Stun") then
-                --     enemy:SetStun(kDisruptMarineTime)
-                -- end
-
-                if HasMixin(enemy, "Webable") then
-                    enemy:SetWebbed(kWebbedDuration, true)
-                end
             end
         end
     -- else
