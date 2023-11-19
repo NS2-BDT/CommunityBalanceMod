f92ed0fe1d401915ce64a4eb11fc701f106f363d
Dreyal <dreyal42@gmail.com>
2023-11-19 14:33:44 +0100

    minimap icon fix

--- src/lua/CommunityBalanceMod/Minimap/NS2Utility.lua
+++ src/lua/CommunityBalanceMod/Minimap/NS2Utility.lua
@@ -4,16 +4,16 @@ local oldBuildClassToGrid = BuildClassToGrid
 function BuildClassToGrid()
     local ClassToGrid = oldBuildClassToGrid()
 
-    ClassToGrid["CommandStationOccupied"] = { 8, 9 }
+    ClassToGrid["CommandStationOccupied"] = { 2, 4 }
     ClassToGrid["WhipMature"] = { 4, 7 }
-    ClassToGrid["JetpackMarine"] = { 7, 9 }
-    ClassToGrid["DrifterEgg"] = { 7, 10 }
-    ClassToGrid["HiveFresh"] = { 1, 10 }
-    ClassToGrid["Hive"] = { 2, 10 }
-    ClassToGrid["HiveMature"] = { 3, 10 }
-    ClassToGrid["HiveFreshOccupied"] = { 4, 10 }
-    ClassToGrid["HiveOccupied"] = { 5, 10 }
-    ClassToGrid["HiveMatureOccupied"] = { 6, 10 }
+    ClassToGrid["JetpackMarine"] = { 3, 2 }
+    ClassToGrid["DrifterEgg"] = { 7, 3 }
+    ClassToGrid["HiveFresh"] = { 1, 6 }
+    ClassToGrid["Hive"] = { 2, 6 }
+    ClassToGrid["HiveMature"] = { 3, 6 }
+    ClassToGrid["HiveFreshOccupied"] = { 5, 2 }
+    ClassToGrid["HiveOccupied"] = { 6, 2 }
+    ClassToGrid["HiveMatureOccupied"] = { 7, 2 }
 
     return ClassToGrid
 end
