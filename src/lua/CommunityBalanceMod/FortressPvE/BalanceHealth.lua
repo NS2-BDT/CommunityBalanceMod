d4ca83f6b74171597a396be3cb985c56be1595f6
Dreyal <dreyal42@gmail.com>
2023-12-23 15:17:26 +0100

    fortress armor into HP

--- src/lua/CommunityBalanceMod/FortressPvE/BalanceHealth.lua
+++ src/lua/CommunityBalanceMod/FortressPvE/BalanceHealth.lua
@@ -1,47 +1,50 @@
 
 
 
--- Fortress PvE
-kCragHealth = 440
-kCragArmor = 140
-kMatureCragHealth = 510
-kMatureCragArmor = 250
 
-kWhipHealth = 600
-kWhipArmor = 160
-kMatureWhipHealth = 660
-kMatureWhipArmor = 220
+--[[
+kCragHealth = 480    
+kCragArmor = 160
+kMatureCragHealth = 560    
+kMatureCragArmor = 272
 
-kShiftHealth = 550
-kShiftArmor = 60
-kMatureShiftHealth = 810
-kMatureShiftArmor = 110
+kWhipHealth = 650    
+kWhipArmor = 175
+kMatureWhipHealth = 720    
+kMatureWhipArmor = 240
 
-kShadeHealth = 550
+kShadeHealth = 600    
 kShadeArmor = 0
-kMatureShadeHealth = 1100
+kMatureShadeHealth = 1200
 kMatureShadeArmor = 0
 
+kShiftHealth = 600    
+kShiftArmor = 60
+kMatureShiftHealth = 880    
+kMatureShiftArmor = 120
+]]
 
-kFortressCragHealth = 480 * 3 
-kFortressCragArmor = 160 * 3 
-kFortressMatureCragHealth = 560 * 3 
-kFortressMatureCragArmor = 272 * 3 
+-- we cant use the constants, since MDS changes them
+
+kFortressCragHealth = 480 * 3 + 160 * 4
+kFortressCragArmor = 160
+kFortressMatureCragHealth = 560 * 3 + 272 * 4
+kFortressMatureCragArmor = 272 
         
-kFortressWhipHealth = 650 * 3 
-kFortressWhipArmor = 175 * 3 
-kFortressMatureWhipHealth = 720 * 3 
-kFortressMatureWhipArmor = 240 * 3 
+kFortressWhipHealth = 650 * 3 + 175 * 4
+kFortressWhipArmor = 175
+kFortressMatureWhipHealth = 720 * 3  + 240 * 4
+kFortressMatureWhipArmor = 240
 
-kFortressShiftHealth = 600 * 3 
-kFortressShiftArmor = 60* 3 
-kFortressMatureShiftHealth = 880 * 3 
-kFortressMatureShiftArmor = 120 * 3 
+kFortressShiftHealth = 600 * 3 + 60 * 4
+kFortressShiftArmor = 60
+kFortressMatureShiftHealth = 880 * 3 + 120 * 4
+kFortressMatureShiftArmor = 120 
 
 kFortressShadeHealth = 600 * 3
-kFortressShadeArmor = 0 * 3
+kFortressShadeArmor = 0
 kFortressMatureShadeHealth = 1200 * 3
-kFortressMatureShadeArmor = 0 * 3
+kFortressMatureShadeArmor = 0
 
 kBalanceOffInfestationHurtPercentPerSecondFortress = 0.02 / 3
 
