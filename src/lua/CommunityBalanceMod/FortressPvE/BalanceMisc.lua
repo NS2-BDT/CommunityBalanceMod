160ec23ad8dec1069aeb8bf66577321d5204e982
twiliteblue <finallyfoundaname@hotmail.com>
2023-12-30 17:37:53 +1100

    Hallucination Drifter and Egg damage multiplier
    Hallucinate Drifter damage multiplier increased to 450%

--- src/lua/CommunityBalanceMod/FortressPvE/BalanceMisc.lua
+++ src/lua/CommunityBalanceMod/FortressPvE/BalanceMisc.lua
@@ -2,3 +2,5 @@ kHallucinationHealthFraction = 1.0 --0.4
 kHallucinationArmorFraction = 1.0 --0.1
 kHallucinationMaxHealth = 2000 --700
 kHallucinationDamageMulti = 3.66 -- hallucinations take more damage
+kHallucinateDrifterDamageMulti = 4.5
+kHallucinateEggDamageMulti = 1.16
