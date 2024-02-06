dc06051b034d880f129f0dca4b80de15f03f5023
Dreyal <dreyal42@gmail.com>
2024-02-06 20:31:07 +0100

    fixed arc selfdmg

--- src/lua/CommunityBalanceMod/Selfharm/NS2Utility.lua
+++ src/lua/CommunityBalanceMod/Selfharm/NS2Utility.lua
@@ -3,7 +3,8 @@
 local oldCanEntityDoDamageTo = CanEntityDoDamageTo
 function CanEntityDoDamageTo(attacker, target, cheats, devMode, friendlyFire, damageType)
    
-    if target:isa("ARC") and attacker:isa("ARC") then
+    -- only arcs deal splash damage. attacker would be marinecommander or marine
+    if target:isa("ARC") and damageType == kDamageType.Splash  then
         return false
     end
 
