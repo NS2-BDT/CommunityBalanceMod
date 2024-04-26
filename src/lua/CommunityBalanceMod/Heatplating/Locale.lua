c62587790485b13db6bb622894f1a8768fa23470
adsfgg <11364579+adsfgg@users.noreply.github.com>
2024-04-26 20:15:45 +0100

    Update locales

--- src/lua/CommunityBalanceMod/Heatplating/Locale.lua
+++ src/lua/CommunityBalanceMod/Heatplating/Locale.lua
@@ -1,6 +1,4 @@
 if Client then
-
-    Locale.substitutions["HEATPLATING"] = "Heat Plating"
-    Locale.substitutions["HEATPLATING_TOOLTIP"] = "Reduced damage from mines, grenades, flames and railguns"
-    
+    Locale.substitutions["HEATPLATING"] = "Adaptive Plating"
+    Locale.substitutions["HEATPLATING_TOOLTIP"] = "Upon taking damage, rapidly replenish energy"
 end
